4d62b22564455ee4163f183c159206bafe18b100
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-27 15:43:16 +0530

    1

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -1,38 +1,48 @@
 package com.driver.controllers;
 
-import com.driver.services.AdminService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.driver.model.*;
+import com.driver.services.AdminService;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	AdminService adminService;
+
 	@PostMapping("/register")
 	public ResponseEntity<Void> registerAdmin(@RequestBody Admin admin){
+		adminService.adminRegister(admin);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@PutMapping("/update")
 	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password){
+		Admin updatedAdmin = adminService.updatePassword(adminId, password);
 		return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete")
 	public void deleteAdmin(@RequestParam Integer adminId){
+		adminService.deleteAdmin(adminId);
 	}
 
 	@GetMapping("/listOfCustomers")
 	public List<Customer> listOfCustomers() {
+		List<Customer> listOfCustomers = adminService.getListOfCustomers();
 		return listOfCustomers;
 	}
 
 	@GetMapping("/listOfDrivers")
 	public List<Driver> listOfDrivers() {
+		List<Driver> listOfDrivers = adminService.getListOfDrivers();
 		return listOfDrivers;
 	}
 }
--- src/main/java/com/driver/repository/TripBookingRepository.java
+++ src/main/java/com/driver/repository/TripBookingRepository.java
@@ -3,6 +3,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TripBookingRepository extends JpaRepository<TripBooking, Integer>{
+public interface TripBookingRepository extends JpaRepository<com.driver.model.TripBooking, Integer>{
 
 }
